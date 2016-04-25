30e48a891b9cb88d0b70b608d71e6beb0d7836fe
Anastasiya Vovk <nastia-vovk@ukr.net>
2016-04-25 12:48:56 +0300

    added home work 3_4 on JS

--- js/script.js
+++ js/script.js
@@ -1,98 +1,88 @@
-var timer = document.querySelector('.timer');
-var digits = document.querySelector('p');
-var miliSec = document.querySelector('span');
-var controlButton = document.querySelector('.start-pause');
-var clearButton = document.querySelector('.clear');
-var watch = new StopWatch(timer);
-
-controlButton.addEventListener('click', startPause);
-function startPause() {
-    if (watch.isOn) {
-      watch.pause();
-      controlButton.innerHTML = 'Continue';
-    } else {
-      watch.start();
-      controlButton.innerHTML = 'Pause';
-    }
+var text = {
+    header: 'Тест по программированию',
+    question: ['Вопрос №1' , 'Вопрос №2' ,' Вопрос №3'],
+    checkboxName: ['one' , 'two' , 'three' , 'four' , 'five' , 'six' , 'seven' ,'eight','nine'],
+    id: ['1' , '2' , '3' , '4' , '5' , '6' , '7' ,'8','9'],
+    answer: ['Вариант ответа №1','Вариант ответа №2', 'Вариант ответа №3',
+            'Вариант ответа №1','Вариант ответа №2', 'Вариант ответа №3',
+            'Вариант ответа №1','Вариант ответа №2', 'Вариант ответа №3'],
+    submit: 'Проверить мои результаты'
 };
+var k = 0;
 
-clearButton.addEventListener('click', clearTimer);
-function clearTimer() {
-    watch.clear();
-    digits.innerHTML = '00:00:00';
-    miliSec.innerHTML = '0';
-    controlButton.innerHTML = 'Start';
+var header = document.createElement ('h3');
+    header.innerHTML = text.header;
+    header.style.marginBottom = '60px';
+    header.style.fontFamily = 'Arial';
+    header.style.textAlign = 'center';
+
+var form = document.createElement( 'form');
+    form.action = '#';
+    form.method = 'post';
+    form.classList.add ('form-group');
+
+var list = document.createElement ('ol');
+    list.classList.add ('list-group');
+    list.style.paddingLeft = '25px';
+
+var parent = document.body;
+
+var container = document.createElement ('div');
+    container.classList.add ('container');
+    container.style.width = '800px';
+    container.style.margin = '0 auto';
+    container.style.padding = '0';
+    container.style.background = '#fff';
+
+var button = document.createElement ('button');
+    button.classList.add ('btn');
+    button.classList.add ('btn-default');
+    button.type = 'submit';
+    button.innerHTML = text.submit;
+    button.style.margin = '20px auto auto 240px';
+    button.style.padding = '10px 30px';
+    button.style.background = '#cfe2f3';
+    button.style.fontSize = '20px';
+    button.style.fontWeight = 'bold';
+
+container.appendChild (header);
+parent.appendChild (container);
+container.appendChild (form);
+form.appendChild (list);
+
+for (var i = 0; i < text.question.length; i++) {
+  var listItem = document.createElement ('li');
+      listItem.classList.add = 'list-group-item';
+      listItem.style.fontSize = '18px';
+      listItem.style.fontFamily = 'Arial';
+
+  list.appendChild (listItem);
+
+  var question = document.createElement ('h4');
+      question.innerHTML = text.question [i];
+
+  listItem.appendChild (question);
+  for (var j = 0; j < 3; j++) {
+    var checkbox = document.createElement ('input');
+      checkbox.type = 'checkbox';
+      checkbox.name = text.checkboxName [k];
+      checkbox.id = text.id [k];
+
+    var answer = document.createElement ('p');
+        answer.style.marginBottom = '0';
+
+    var label = document.createElement ('label');
+      label.htmlFor = text.id [k];
+      label.style.marginLeft = '10px';
+      label.style.fontSize = '18px';
+      label.style.fontWeight = 'normal';
+
+    label.appendChild (document.createTextNode(text.answer [k]));
+    listItem.appendChild (answer);
+    answer.appendChild (checkbox);
+    answer.appendChild (label);
+    k++;
+  }
 }
 
-//конструктор для часов
-function StopWatch(elem) {
-    var time = 0;
-    var interval;
-    var startTime;
-    var hour;
-    var min;
-    var sec;
-    var msec;
-
-  //функция определения времени с учетом прошедшего интервала
-    function update() {
-        time += delta();
-        timeFormat(time);
-        var globTime = hour + ':' + min + ':' + sec;
-        elem.querySelector('p').innerHTML = globTime;
-        elem.querySelector('span').innerHTML = msec;
-    };
-
-  //функция определения количества прошедшео времени
-    function delta() {
-        var now = Date.now();
-        var timePassed = now - startTime;
-        startTime = now;
-        return timePassed;
-    };
-
-  //функция вывода времени в нужном формате
-    function timeFormat(timeInMSec) {
-        var newTime = new Date(timeInMSec);
-        msec = newTime.getMilliseconds().toString();
-        sec = newTime.getSeconds().toString();
-        min = newTime.getMinutes();
-        hour = (Math.floor(min/60)).toString();
-        min = min.toString();
-
-        if (hour.length < 2) {
-          hour = '0' + hour;
-        }
-        if (min.length < 2) {
-          min = '0' + min;
-        }
-        if (sec.length < 2) {
-          sec = '0' + sec;
-        }
-        while (msec.length < 3) {
-          msec = '0' + msec;
-        }
-        return hour, min, sec, msec;
-    };
-
-    this.isOn = false; //переключатель между "старт/пауза"
-    this.start = function() {
-        if (!this.isOn) {
-          interval = setInterval(update, 10);
-          startTime = Date.now();
-          this.isOn = true;
-        }
-    };
-
-    this.pause = function() {
-        if (this.isOn) {
-          clearInterval(interval);
-          interval = 0;
-          this.isOn = false;
-        }
-    };
-
-    this.clear = function() {
-        time = 0;
-    };
-}
+form.appendChild (button);
